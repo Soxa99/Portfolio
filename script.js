762ceb34beb0c2fbe6a8a0894bb45f195f5640c6
Adrian Socha <adriansocha1999@gmail.com>
2019-08-13 19:48:39 +0200

    Change animation burger

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
 $('.burger').on('click', function () {
-    // if ($('div').hasClass('close') == true) {
-    //     $('aside').animate({
-    //         right: '-300px',
-    //     }, {
-    //         duration: 500,
-    //         specialEasing: {
-    //             right: "linear",
-    //         }
-    //     });
-    // } else {
-    //     $('aside').animate({
-    //         right: 0,
-    //     }, {
-    //         duration: 500,
-    //         specialEasing: {
-    //             right: "linear",
-    //         }
-    //     });
-    // }
     $('.burger').toggleClass("active");
 })
